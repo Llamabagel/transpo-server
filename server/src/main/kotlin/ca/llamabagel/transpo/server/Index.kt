ab5ce33e80873da532cbada395ab3ee2e98e3f57
derek <derek@llamabagel.ca>
2018-10-30 22:10:26 -0400

    Added a method to include the required API keys in a configuration file that can then be accessed via a Kotlin object.
    Added .gitignore.

--- server/src/main/kotlin/ca/llamabagel/transpo/server/Index.kt
+++ server/src/main/kotlin/ca/llamabagel/transpo/server/Index.kt
@@ -1,6 +1,8 @@
 package ca.llamabagel.transpo.server
 
 import io.ktor.application.call
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.Route
@@ -8,6 +10,8 @@ import io.ktor.routing.get
 
 fun Route.index() {
     get {
-        call.respondText("Hello World!", ContentType.Text.Plain)
+        val client = HttpClient(Apache)
+
+        call.respondText("Hello World! ${Keys.OC_TRANSPO_APP_ID}", ContentType.Text.Plain)
     }
 }
---
+++ server/src/main/kotlin/ca/llamabagel/transpo/server/Keys.kt
@@ -0,0 +1,41 @@
+package ca.llamabagel.transpo.server
+
+import java.io.FileInputStream
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
+
+/**
+ * Object to access various API keys configured in the project setup.
+ * The various keys are loaded in from the keys.properties file located in the server module.
+ *
+ * @property OC_TRANSPO_APP_ID The OC Transpo app id required to access their API.
+ * @property OC_TRANSPO_API_KEY The API key associated with the App id used to access the OC Transpo API.
+ */
+object Keys {
+    val OC_TRANSPO_APP_ID: String
+    var OC_TRANSPO_API_KEY: String
+
+    init {
+        if (!Files.exists(Paths.get("server/keys.properties"))) {
+            throw IOException("keys.properties file not found.")
+        }
+
+        val properties = Properties().apply {
+            load(FileInputStream("server/keys.properties"))
+        }
+
+        if (properties.contains("OC_TRANSPO_APP_ID")) {
+            OC_TRANSPO_APP_ID = properties.getProperty("OC_TRANSPO_APP_ID")
+        } else {
+            throw IllegalStateException("OC Transpo App Id not set in keys.properties file.")
+        }
+
+        if (properties.contains("OC_TRANSPO_API_KEY")) {
+            OC_TRANSPO_API_KEY = properties.getProperty("OC_TRANSPO_API_KEY")
+        } else {
+            throw IllegalStateException("OC Transpo API key not set in keys.properties file.")
+        }
+    }
+}
