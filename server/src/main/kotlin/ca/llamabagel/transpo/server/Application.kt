6e81786b781cb88af3cb23e48e3412d78ac9d4ac
derek <derek@llamabagel.ca>
2019-05-04 19:16:59 -0400

    App data API endpoints

--- server/src/main/kotlin/ca/llamabagel/transpo/server/Application.kt
+++ server/src/main/kotlin/ca/llamabagel/transpo/server/Application.kt
@@ -4,24 +4,28 @@
 
 package ca.llamabagel.transpo.server
 
+import ca.llamabagel.transpo.server.data.data
 import ca.llamabagel.transpo.server.plans.plans
 import ca.llamabagel.transpo.server.trips.trips
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
 import io.ktor.http.ContentType
 import io.ktor.routing.Routing
 
 fun Application.main() {
     install(DefaultHeaders)
     install(ContentNegotiation) {
-        register(ContentType.Application.Json, JsonSerializableConverter())
+        //register(ContentType.Any, JsonSerializableConverter())
+        gson {  }
     }
 
     install(Routing) {
         index()
         trips()
         plans()
+        data()
     }
 }
---
+++ server/src/main/kotlin/ca/llamabagel/transpo/server/data/Data.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2019 Derek Ellis. Subject to the MIT license.
+ */
+
+package ca.llamabagel.transpo.server.data
+
+import ca.llamabagel.transpo.Configuration
+import ca.llamabagel.transpo.models.app.AppMetadata
+import ca.llamabagel.transpo.models.app.DataPackage
+import ca.llamabagel.transpo.models.app.MetadataRequest
+import ca.llamabagel.transpo.models.app.Version
+import ca.llamabagel.transpo.server.DataSource
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.response.respondFile
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.post
+import java.io.File
+
+fun Routing.data() {
+
+
+    post("data/metadata") {
+        val requestData = call.receive<MetadataRequest>()
+
+        val metaData = getMetadata(requestData.platform)
+        if (metaData != null) {
+            call.respond(metaData)
+        } else {
+            call.respond(HttpStatusCode.InternalServerError, Any())
+        }
+    }
+
+    get("data/{platform}/{schema}/{v?}") {
+        var version = context.parameters["v"]
+        val schema = context.parameters["schema"]?.toIntOrNull() ?: 1
+        val platform = context.parameters["platform"]!!
+
+        if (version == null) {
+            val metadata = getMetadata(platform)
+            if (metadata == null) {
+                call.respond(HttpStatusCode.InternalServerError, Any())
+                return@get
+            }
+
+            version = metadata.dataVersion.value
+        }
+
+        val file = File("${Configuration.DATA_PACKAGE_DIRECTORY}/$schema/$version/$version.json")
+        if (file.exists()) {
+            call.respondFile(file)
+        } else {
+            call.respond(HttpStatusCode.NotFound, Any())
+        }
+    }
+}
